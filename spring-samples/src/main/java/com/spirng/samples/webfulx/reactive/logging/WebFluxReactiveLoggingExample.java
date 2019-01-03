e1c3eadbfba2bca2a8655b74a7a8a6522751415b
Carlos Andres De La Rosa <rosacar@afis-mgmt.local>
2019-01-03 12:15:04 +0100

    Logging a reactive stream using spring webflux example

---
+++ spring-samples/src/main/java/com/spirng/samples/webfulx/reactive/logging/WebFluxReactiveLoggingExample.java
@@ -0,0 +1,36 @@
+package com.spirng.samples.webfulx.reactive.logging;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * This class shows how to use logging in a reactive stream
+ * using Spring webflux 
+ * @author carlosdlr
+ *
+ */
+public class WebFluxReactiveLoggingExample {
+
+
+	public static void main(String [] args) {
+
+		//creates a reactive stream using spring flux and enable logging using the log method
+		Flux<Integer> reactiveStream = Flux.range(1, 5).log();
+
+		//now we subscribe to the to this stream to consume the generated values 
+		reactiveStream.subscribe();
+
+		//using the take approach we can say to flux that just take a specific number of events in the stream.
+		//the take method also cancel the stream after 3 events
+		reactiveStream = Flux.range(1, 5).log().take(3);
+
+		//now we subscribe to the to this stream to consume the generated values 
+		reactiveStream.subscribe();
+		
+        //with this approach the stream will complete this is because we observe the output of using take() instead of what was requested by this method.
+		reactiveStream = Flux.range(1, 5).take(3).log();
+		
+		//now we subscribe to the to this stream to consume the generated values 
+		reactiveStream.subscribe();
+
+	}
+}
